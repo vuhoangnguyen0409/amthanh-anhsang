bad75013e000a6ab7798755f2dbccb4e2aaf4262
Vu Hoang Nguyen <vuhoangnguyen49@gmail.com>
2021-03-20 10:55:23 +0700

    homepage - query category

--- wp-content/themes/amthanh-anhsang/front-page.php
+++ wp-content/themes/amthanh-anhsang/front-page.php
@@ -8,126 +8,51 @@
 			<div id="fs-scroll" class="fs-scroll--h">
 			   <div id="fs-scroll__content">
 				  <div class="albums-grid grid-albums-horizontal">
-					 <div class="scroll-item is-waypoint fx-scale-from-right delay-05">
-						<div class="mod mod-album-2 album-item">
-						   <div class="mod__inner post-254 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
-							  <div class="mod__img">
-								 <div class="ithumb ithumb-perspective">
-									<a href="/" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
-									  <div class="ithumb-perspective__img">
-										  <div class="img waypoint-delay">
-											 <img src="<?php bloginfo('template_directory');?>/images/uploads/citylife.jpg" alt="Post Image" class="fx-load-img">
-										  </div>
-									   </div>
-									   <div class="mod-title ithumb-perspective__title h2-line">
-										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06">City Life</span></h2>
-									   </div>
-									</a>
-								 </div>
-							  </div>
-						   </div>
-						</div>
-					 </div>
-					 <div class="scroll-item is-waypoint fx-scale-from-right delay-05">
-						<div class="mod mod-album-2 album-item">
-						   <div class="mod__inner post-246 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
-							  <div class="mod__img">
-								 <div class="ithumb ithumb-perspective">
-									<a href="/" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
-									   <div class="mod-title ithumb-perspective__title">
-										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06">Submission</span></h2>
-									   </div>
-									   <div class="ithumb-perspective__img">
-										  <div class="img waypoint-delay">
-											 <img src="<?php bloginfo('template_directory');?>/images/uploads/Submissione.jpg" alt="Post Image" class="fx-load-img">
-										  </div>
-									   </div>
-									</a>
-								 </div>
-							  </div>
-						   </div>
-						</div>
-					 </div>
-					 <div class="scroll-item is-waypoint fx-scale-from-right delay-05">
-						<div class="mod mod-album-2 album-item">
-						   <div class="mod__inner post-257 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
-							  <div class="mod__img">
-								 <div class="ithumb ithumb-perspective">
-									<a href="/" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
-									   <div class="mod-title ithumb-perspective__title">
-										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06">Space Trap</span></h2>
-									   </div>
-									   <div class="ithumb-perspective__img">
-										  <div class="img waypoint-delay">
-											 <img src="<?php bloginfo('template_directory');?>/images/uploads/spacetrap2.jpg" alt="Post Image" class="fx-load-img">
-										  </div>
-									   </div>
-									</a>
-								 </div>
-							  </div>
-						   </div>
-						</div>
-					 </div>
-					 <div class="scroll-item is-waypoint fx-scale-from-right delay-05">
-						<div class="mod mod-album-2 album-item">
-						   <div class="mod__inner post-252 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
-							  <div class="mod__img">
-								 <div class="ithumb ithumb-perspective">
-									<a href="/" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
-									   <div class="mod-title ithumb-perspective__title">
-										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06">Call To Mind</span></h2>
-									   </div>
-									   <div class="ithumb-perspective__img">
-										  <div class="img waypoint-delay">
-											 <img src="<?php bloginfo('template_directory');?>/images/uploads/Calltomind-1.jpg" alt="Post Image" class="fx-load-img">
-										  </div>
-									   </div>
-									</a>
-								 </div>
-							  </div>
-						   </div>
-						</div>
-					 </div>
-					 <div class="scroll-item is-waypoint fx-scale-from-right delay-05">
-						<div class="mod mod-album-2 album-item">
-						   <div class="mod__inner post-179 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
-							  <div class="mod__img">
-								 <div class="ithumb ithumb-perspective">
-									<a href="/" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
-									   <div class="mod-title ithumb-perspective__title">
-										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06">Astroworld</span></h2>
-									   </div>
-									   <div class="ithumb-perspective__img">
-										  <div class="img waypoint-delay">
-											 <img src="<?php bloginfo('template_directory');?>/images/uploads/Astroworld-3.jpg" alt="Post Image" class="fx-load-img">
-										  </div>
-									   </div>
-									</a>
-								 </div>
-							  </div>
-						   </div>
-						</div>
-					 </div>
-					 <div class="scroll-item is-waypoint fx-scale-from-right delay-05">
-						<div class="mod mod-album-2 album-item">
-						   <div class="mod__inner post-240 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
-							  <div class="mod__img">
-								 <div class="ithumb ithumb-perspective">
-									<a href="/" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
-									   <div class="mod-title ithumb-perspective__title">
-										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06">Empire State</span></h2>
-									   </div>
-									   <div class="ithumb-perspective__img">
-										  <div class="img waypoint-delay">
-											 <img src="<?php bloginfo('template_directory');?>/images/uploads/empirestate.jpg" alt="Post Image" class="fx-load-img">
-										  </div>
-									   </div>
-									</a>
-								 </div>
-							  </div>
-						   </div>
-						</div>
-					 </div>
+
+						<?php $args = array(
+								'posts_per_page'   => 10,
+								'offset'           => 0,
+								'category'         => 'amthanh-anhsang',
+								'category_name'    => '',
+								'orderby'          => 'post_date',
+								'order'            => 'ASC',
+								//'order'            => 'DESC',
+								'include'          => '',
+								'exclude'          => '',
+								'meta_key'         => '',
+								'meta_value'       => '',
+								//'post_type'        => 'news',
+								'post_type'        => '',
+								'post_mime_type'   => '',
+								'post_parent'      => '',
+								'post_status'      => 'publish',
+								'suppress_filters' => true
+							);
+							$posts_array = get_posts( $args );
+		        	foreach ( $posts_array as $post ) : setup_postdata( $post );
+								$thumbnail_id = get_post_thumbnail_id( $post->ID );
+						    $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);?>
+								<div class="scroll-item is-waypoint fx-scale-from-right delay-05">
+		 						<div class="mod mod-album-2 album-item">
+		 						   <div class="mod__inner post-254 vex_albums type-vex_albums status-publish has-post-thumbnail hentry">
+		 							  <div class="mod__img">
+		 								 <div class="ithumb ithumb-perspective">
+		 									<a href="<?php the_permalink(); ?>" class="fx-cursor fx-load fx-load-fi" data-cursor-class="click">
+		 									  <div class="ithumb-perspective__img">
+		 										  <div class="img waypoint-delay">
+		 											 <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo $alt; ?>" class="fx-load-img">
+		 										  </div>
+		 									   </div>
+		 									   <div class="mod-title ithumb-perspective__title h2-line">
+		 										  <h2 class="mod-title__h fx-txt-b"><span class="fx-txt-b__text delay-06"><?php the_title() ?></span></h2>
+		 									   </div>
+		 									</a>
+		 								 </div>
+		 							  </div>
+		 						   </div>
+		 						</div>
+		 					 </div>
+							<?php endforeach; ?>
 				  </div>
 			   </div>
 			   <!-- #fs-scroll__content -->
