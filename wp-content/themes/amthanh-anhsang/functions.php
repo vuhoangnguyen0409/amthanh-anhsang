1aaf0ae320dae5d52fda22abcc0b41722d3887dd
vuhoangnguyen0409 <30582057+vuhoangnguyen0409@users.noreply.github.com>
2021-03-24 16:34:38 +0700

    Add Them General Setting

--- wp-content/themes/amthanh-anhsang/functions.php
+++ wp-content/themes/amthanh-anhsang/functions.php
@@ -164,4 +164,16 @@ function timkiem() {
     echo($response);
     die();
 }
+
+/************ Theme Options************/
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+}
 ?>
